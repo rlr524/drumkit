c903bac49145468d5d2b5b2bf42c21ae3567a272
Rob Ranf <robranf@gmail.com>
2019-10-01 20:36:31 -0700

    Adding background images and sounds

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// create an event listener for the first button with an anonymous function that generates the alert when the button is clicked
+// create an event listener for all seven buttons with a function that generates the sound files
+var audio = new Audio("sounds/crash.mp3");
 let numberDrumButtons = document.querySelectorAll(".drum").length;
 for (let i = 0; i<numberDrumButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked");
-    });
+    document.querySelectorAll(".drum")[i].addEventListener("click", gotClicked);
+}
+function gotClicked() {
+    this.style.color = "white";
 }
